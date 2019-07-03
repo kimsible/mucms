836911171998619c17809efc9b1277d2dfd86eb6
kimsible <kimsible@users.noreply.github.com>
2019-07-03 18:59:11 +0200

    feat(server/static): add static opener

--- src/server/__tests__/unit.js
+++ src/server/__tests__/unit.js
@@ -1,9 +1,13 @@
 'use strict'
 
 import test from 'ava'
-import { request, createServer } from 'http'
 
+import { request, createServer } from 'http'
 import body from '../body'
+
+import { resolve } from 'path'
+import { promises as fs } from 'fs'
+import { create as createStatic, open as openStatic } from '../static'
 
 test('body', bodyTest, 'my-body', 'my-body')
 
@@ -24,4 +28,14 @@ async function bodyTest (t, input, expected) {
   })
 
   t.deepEqual(await body(res), expected)
+}
+
+test('static', staticTest, { url: '/img/logo.1234.png', contentType: 'image/png' })
+
+async function staticTest (t, input) {
+  const cwd = resolve(__dirname, '../../../test/fixtures')
+  createStatic(cwd)
+  const { contentType, data } = await openStatic({ url: input.url })
+  t.regex(contentType, new RegExp(input.contentType))
+  t.deepEqual(data, await fs.readFile(cwd + input.url))
 }
---
+++ src/server/static.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { promises: fs } = require('fs')
+const { contentType, lookup } = require('mime-types')
+
+let path
+const create = cwd => { path = cwd }
+
+const open = async ({ url }) => {
+  const match = url.match(new RegExp(`^/(css|js|img|favicon)`))
+  if (match) {
+    const filepath = path + url
+    const data = await fs.readFile(filepath)
+    return { data, contentType: contentType(lookup(filepath)) }
+  }
+}
+
+module.exports = {
+  create,
+  open
+}
